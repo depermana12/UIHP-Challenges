a95134dcf8a9023a2dcc1a434257bf64fe35d04b
depermana12 <deddiapermana97@gmail.com>
2024-08-25 20:13:53 +0700

    part 1 and 2

--- challenges/part1.js
+++ challenges/part1.js
@@ -1,1 +1,13 @@
 /** Write your code below */
+
+let post = "";
+
+const jsInput = document.querySelector("input");
+const jsDiv = document.querySelector("div");
+
+const handleInput = () => {
+  post = jsInput.value;
+  jsDiv.textContent = post;
+};
+
+jsInput.oninput = handleInput;
--- challenges/part2.js
+++ challenges/part2.js
@@ -1,11 +1,48 @@
-let post = '';
+/** Write your code below */
 
-const jsInput = document.querySelector('input');
-const jsDiv = document.querySelector('div');
+let posts = [];
+let post;
 
-function handleInput() {
+const jsInput = document.querySelector("input");
+const jsDiv = document.querySelector("div");
+const submitButton = document.createElement("button");
+submitButton.textContent = "Submit";
+jsDiv.appendChild(submitButton);
+
+const handleInput = () => {
   post = jsInput.value;
-  jsDiv.textContent = post;
-}
+};
+
+const dataToView = () => {
+  if (post === undefined) {
+    jsInput.value = "What's on your mind";
+  }
+
+  if (posts.length > 0) {
+    const postList = document.querySelectorAll(".post");
+    postList.forEach((post) => post.remove());
+  }
+
+  posts.forEach((postContent) => {
+    const postDiv = document.createElement("div");
+    postDiv.textContent = postContent;
+    postDiv.classList.add("post");
+    jsDiv.appendChild(postDiv);
+  });
+};
+
+const createPost = () => {
+  if (post !== "") {
+    posts.push(post);
+    // reset
+    post = "";
+    jsInput.value = "";
+    dataToView();
+  }
+};
 
 jsInput.oninput = handleInput;
+submitButton.onclick = createPost;
+
+dataToView();
+// setInterval(dataToView, 15);
